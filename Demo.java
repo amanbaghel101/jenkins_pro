e6a89bc3c219585edafb55a2ddf2b71b7e4f1d36
aman baghel <amanbaghel333@gmail.com>
2024-02-26 11:36:14 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,7 +1,7 @@
 class Demo{
 	public static void main(String [] args){
-		system.out.println("hii");
-		system.out.println("aman");
+		System.out.println("hii");
+		System.out.println("aman");
 	}
 }
 
